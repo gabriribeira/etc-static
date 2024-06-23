9d40bfab97aace2f84cf76bf161116da8338ab2b
Lara Mendes <laramendes@ua.pt>
2024-06-23 14:57:56 +0100

    alteracoes header

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -25,7 +25,7 @@ function Features() {
 
       <div className="container mx-auto px-4 lg:pt-12 md:pb-12">
         <div className="flex flex-col lg:flex-row items-start lg:items-center lg:space-x-8 mt-12 lg:mt-0">
-          <div className="flex flex-col w-full lg:w-1/2 mb-8 lg:mb-0">
+          <div className="flex flex-col w-full lg:w-1/2 mb-8 lg:mb-0 text-center lg:text-left">
             <div className="mb-4">
               <h3 className="text-2xl lg:text-3xl font-semibold">Collaborative Shopping Lists</h3>
               <p className="text-lg lg:text-xl">
@@ -33,10 +33,10 @@ function Features() {
               </p>
             </div>
             <div className="flex flex-wrap justify-center lg:justify-start space-x-4">
-              <img src={collaborativeImage1} alt="Collaborative 1" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105" />
-              <img src={collaborativeImage2} alt="Collaborative 2" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105" />
-              <img src={collaborativeImage3} alt="Collaborative 3" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105" />
-              <img src={collaborativeImage4} alt="Collaborative 4" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105" />
+              <img src={collaborativeImage1} alt="Collaborative 1" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105 hidden md:block" />
+              <img src={collaborativeImage2} alt="Collaborative 2" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105 hidden md:block" />
+              <img src={collaborativeImage3} alt="Collaborative 3" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105 hidden md:block" />
+              <img src={collaborativeImage4} alt="Collaborative 4" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105 hidden md:block" />
             </div>
           </div>
         </div>
@@ -44,16 +44,16 @@ function Features() {
 
       <div className="container mx-auto px-4 lg:pt-12 md:pt-2">
         <div className="flex flex-col lg:flex-row items-start lg:items-center lg:space-x-8 mt-12 lg:mt-0">
-          <div className="flex flex-col w-full lg:w-1/2 mb-8 lg:mb-0">
+          <div className="flex flex-col w-full lg:w-1/2 mb-8 lg:mb-0 text-center lg:text-right">
             <div className="mb-4">
-              <h3 className="text-2xl lg:text-3xl font-semibold text-right">AI-powered Shopping Lists</h3>
-              <p className="text-lg lg:text-xl text-right">
+              <h3 className="text-2xl lg:text-3xl font-semibold">AI-powered Shopping Lists</h3>
+              <p className="text-lg lg:text-xl">
                 Ask AI to automatically generate shopping lists for your favorite recipes or events. Get everything you need in a matter of seconds!
               </p>
             </div>
             <div className="flex flex-wrap justify-center lg:justify-end space-x-4">
-              <img src={aiImage1} alt="AI 1" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105" />
-              <img src={aiImage2} alt="AI 2" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105" />
+              <img src={aiImage1} alt="AI 1" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105 hidden md:block" />
+              <img src={aiImage2} alt="AI 2" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105 hidden md:block" />
             </div>
           </div>
         </div>
@@ -61,7 +61,7 @@ function Features() {
 
       <div className="container mx-auto px-4 lg:pt-12 md:pt-2">
         <div className="flex flex-col lg:flex-row items-start lg:items-center lg:space-x-8 mt-12 lg:mt-0">
-          <div className="flex flex-col w-full lg:w-1/2 mb-8 lg:mb-0">
+          <div className="flex flex-col w-full lg:w-1/2 mb-8 lg:mb-0 text-center lg:text-left">
             <div className="mb-4">
               <h3 className="text-2xl lg:text-3xl font-semibold">Manage Expenses</h3>
               <p className="text-lg lg:text-xl">
@@ -69,8 +69,8 @@ function Features() {
               </p>
             </div>
             <div className="flex flex-wrap justify-center lg:justify-start space-x-4">
-              <img src={expensesImage1} alt="Expenses 1" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105" />
-              <img src={expensesImage2} alt="Expenses 2" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105" />
+              <img src={expensesImage1} alt="Expenses 1" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105 hidden md:block" />
+              <img src={expensesImage2} alt="Expenses 2" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105 hidden md:block" />
             </div>
           </div>
         </div>
@@ -78,16 +78,16 @@ function Features() {
 
       <div className="container mx-auto px-4 lg:pt-12 md:pt-2">
         <div className="flex flex-col lg:flex-row items-start lg:items-center lg:space-x-8 mt-12 lg:mt-0">
-          <div className="flex flex-col w-full lg:w-1/2 mb-8 lg:mb-0">
+          <div className="flex flex-col w-full lg:w-1/2 mb-8 lg:mb-0 text-center lg:text-right">
             <div className="mb-4">
-              <h3 className="text-2xl lg:text-3xl font-semibold text-right">Sustainable Goals</h3>
-              <p className="text-lg lg:text-xl text-right">
+              <h3 className="text-2xl lg:text-3xl font-semibold">Sustainable Goals</h3>
+              <p className="text-lg lg:text-xl">
                 Initiate a sustainable goal and have every household member contribute to a greener environment.
               </p>
             </div>
             <div className="flex flex-wrap justify-center lg:justify-end space-x-4">
-              <img src={sustainableImage1} alt="Sustainable 1" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105" />
-              <img src={sustainableImage2} alt="Sustainable 2" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105" />
+              <img src={sustainableImage1} alt="Sustainable 1" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105 hidden md:block" />
+              <img src={sustainableImage2} alt="Sustainable 2" className="w-full max-w-xs lg:max-w-sm mb-4 transition-transform duration-300 transform hover:scale-105 hidden md:block" />
             </div>
           </div>
         </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from '../imgs/logo.png';
 import phoneImage from '../imgs/placeholder.png';
 import backgroundImage from '../imgs/Ellipse.png';
+import '../index.css'; // Importe o arquivo CSS para estilos
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -13,58 +14,62 @@ function Header() {
     if (targetElement) {
       targetElement.scrollIntoView({ behavior: 'smooth' });
     }
-    setMenuOpen(false);
+    setMenuOpen(false); // Feche o menu após clicar em um link
   };
 
   const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+    setMenuOpen(!menuOpen); // Alternar estado do menu
   };
 
   return (
-    
-      <header className="relative bg-black text-white pt-8 fixed" id="top">
-        <div className="container mx-auto px-4 flex items-center justify-between">
-          <img src={logo} alt="et.cetera" className="h-6 w-auto" />
+    <header className="relative bg-black text-white pt-8 fixed w-full" id="top">
+      <div className="container mx-auto px-4 flex items-center justify-between">
+        <img src={logo} alt="et.cetera" className="h-6 w-auto" />
 
-          <div className="relative">
-            <button onClick={toggleMenu} className="md:hidden text-white focus:outline-none">
+        <div className="relative">
+          <button onClick={toggleMenu} className="md:hidden text-white focus:outline-none">
+            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+          <div className={`mobile-menu ${menuOpen ? 'open' : ''}`}>
+            <button className="close-button" onClick={toggleMenu}>
               <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
               </svg>
             </button>
-            {menuOpen && (
-              <div className="absolute left-[-200px] mt-2 bg-black text-white py-2">
-                <div className="flex justify-around gap-x-4">
-                  <a href="#features" onClick={handleClick} className="nav-link hover:text-gray-400 hover:underline">Features</a>
-                  <a href="#about" onClick={handleClick} className="nav-link hover:text-gray-400 hover:underline">About</a>
-                  <a href="#contact" onClick={handleClick} className="nav-link hover:text-gray-400 hover:underline">Contact</a>
-                </div>
-              </div>
-            )}
+            <div className="flex flex-col items-start gap-y-4">
+              <a href="#features" onClick={handleClick} className="nav-link hover:text-gray-400 hover:underline">Features</a>
+              <a href="#about" onClick={handleClick} className="nav-link hover:text-gray-400 hover:underline">About</a>
+              <a href="#contact" onClick={handleClick} className="nav-link hover:text-gray-400 hover:underline">Contact</a>
+            </div>
           </div>
+        </div>
 
-          <nav className="hidden md:flex md:items-center md:space-x-8">
-            <a href="#features" onClick={handleClick} className="nav-link hover:text-gray-400 hover:underline">Features</a>
-            <a href="#about" onClick={handleClick} className="nav-link hover:text-gray-400 hover:underline">About</a>
-            <a href="#contact" onClick={handleClick} className="nav-link hover:text-gray-400 hover:underline">Contact</a>
-          </nav>
+        <nav className="hidden md:flex md:items-center md:space-x-8">
+          <a href="#features" onClick={handleClick} className="nav-link hover:text-gray-400 hover:underline">Features</a>
+          <a href="#about" onClick={handleClick} className="nav-link hover:text-gray-400 hover:underline">About</a>
+          <a href="#contact" onClick={handleClick} className="nav-link hover:text-gray-400 hover:underline">Contact</a>
+        </nav>
+      </div>
+      <div className="text-center mt-16 md:mt-32 relative">
+        <h1 className="text-4xl mb-4 px-4 font-semibold">The household management app</h1>
+        <p className="text-md md:text-lg mb-8 px-4">that revolutionizes shared-home living</p>
+        <div className="relative flex justify-center">
+          <img src={phoneImage} alt="Phone" className="h-auto z-10 lg:w-[800px] lg:h-auto" />
+          <img src={backgroundImage} alt="Background" className="absolute inset-0 w-full h-full z-0 object-contain mt-[200px] md:mt-[200px] lg:mt-[300px] lg:ml-4" />
         </div>
-        <div className="text-center mt-16 md:mt-32 relative">
-          <h1 className="text-3xl md:text-4xl mb-4">The household management app</h1>
-          <p className="text-md md:text-lg mb-8">that revolutionizes shared-home living</p>
-          <div className="relative flex justify-center">
-            <img src={phoneImage} alt="Phone" className="w-full max-w-xs md:max-w-lg h-auto z-10" />
-            <img src={backgroundImage} alt="Background" className="absolute inset-0 w-full h-full z-0 object-cover mt-20 md:mt-44" />
+      </div>
+      <div className="bg-white py-20 md:py-44">
+        <div className="container mx-auto px-4 flex flex-col md:flex-row">
+          <div className="w-full md:w-1/2"></div>
+          <div className="w-full md:w-1/2 text-center md:text-right">
+            <p className="text-black lg:text-xl font-semibold text-center md:text-right">The solution for all your household-related problems</p>
+            <p className="text-black lg:text-xl font-normal mt-2 text-center md:text-right">No more forgetting to buy products, bringing the wrong items home, or having a housemate owe you money.</p>
           </div>
         </div>
-        <div className="bg-white py-20 md:py-44">
-          <div className="container mx-auto px-4 text-center md:text-right">
-            <p className="text-black font-medium"><strong>The solution for all your household-related problems</strong></p>
-            <p className="text-black font-normal mt-2">No more forgetting to buy products, bringing the wrong items home, or having a housemate owe you money.</p>
-          </div>
-        </div>
-      </header>
-    
+      </div>
+    </header>
   );
 }
 
